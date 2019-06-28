3a9d0e99ef69712bf51726d1de072ade3dae5a8a
Sidhant Puntambekar <35539945+SidhantPuntambekar@users.noreply.github.com>
2019-06-28 10:14:44 -0600

    First iteration of the gcPercentage program

---
+++ gcPercent.py
@@ -0,0 +1,16 @@
+""" A program to compute the gc percentage of a given DNA sequence 
+"""
+
+# get DNA sequence:
+dna = 'acgctcgcgcggcgatagctgatcgatcggcgcgctttttttttaaaag'
+no_a = dna.count('a')
+no_c = dna.count('c')
+no_g = dna.count('g')
+no_t = dna.count('t')
+dna_length = len(dna)
+gc_percent = (no_c+no_g)*100.0/dna_length
+at_percent = (no_a+no_t)*100.0/dna_length
+#printStringAT = "The A-T percentage of this DNA sequence is: "
+#printStringGC = "The G-C percentage of this DNA sequence is: "
+#print(printStringAT + str(at_percent))
+print("""printStringGC""" + str(gc_percent))
