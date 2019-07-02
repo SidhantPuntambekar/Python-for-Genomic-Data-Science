fa262947ea2721aa77e080fe93e236a4ec046f1c
Sidhant Puntambekar <35539945+SidhantPuntambekar@users.noreply.github.com>
2019-07-01 20:57:12 -0600

    Fixed styling issues

--- gcPercent.py
+++ gcPercent.py
@@ -8,9 +8,9 @@ no_c = dna.count('c')
 no_g = dna.count('g')
 no_t = dna.count('t')
 dna_length = len(dna)
-gc_percent = (no_c+no_g)*100.0/dna_length
-at_percent = (no_a+no_t)*100.0/dna_length
+gc_percentage = (no_c+no_g)*100.0/dna_length
+at_percentage = (no_a+no_t)*100.0/dna_length
 printStringAT = "The A-T percentage of this DNA sequence is: "
 printStringGC = "The G-C percentage of this DNA sequence is: "
-print(printStringAT + str(at_percent))
-print(printStringGC + str(gc_percent))
+print(printStringAT + str(at_percentage))
+print(printStringGC + str(gc_percentage))
