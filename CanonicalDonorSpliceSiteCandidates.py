fde84f08db58f4ceda2d0a6829211505baa2d9a6
Sidhant Puntambekar <35539945+SidhantPuntambekar@users.noreply.github.com>
2019-07-02 09:53:01 -0600

    Added while loop and print statement

--- CanonicalDonorSpliceSiteCandidates.py
+++ CanonicalDonorSpliceSiteCandidates.py
@@ -3,3 +3,6 @@
 dna = input('Enter a DNA Sequence: ')
 #A canonical donor splce site will always start with the base pair sequence GT
 pos = dna.find('gt', 0)
+while (pos>1) :
+    print("Donor splice site at position %d"% pos)
+    
