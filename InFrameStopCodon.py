a303c04d7af80eb71f869c13f4e9bf7a58390715
Sidhant Puntambekar <35539945+SidhantPuntambekar@users.noreply.github.com>
2019-07-03 10:10:26 -0600

    Added frame reference variable

--- InFrameStopCodon.py
+++ InFrameStopCodon.py
@@ -1,14 +1,15 @@
 dna = input("Enter a DNA sequence:")
-def has_stop_codon(dna) :
+def has_stop_codon(dna,frame) :
     "This function checks if dna sequence has an in frame stop codon"
     stop_codon_found = False
     stop_codons = ['tga', 'tag', 'taa']
-    for i in range(0,len(dna),3) :
+    for i in range(frame,len(dna),3) :
         codon = dna[i:i+3].lower()
         if codon in stop_codons :
             stop_codon_found = True
             break
     return stop_codon_found
+
 if(has_stop_codon(dna))
     print("Input sequence has a stop codon")
 else:
