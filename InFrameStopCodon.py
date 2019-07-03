b88f85ed1b112a527ef324b7923189ec1f581416
Sidhant Puntambekar <35539945+SidhantPuntambekar@users.noreply.github.com>
2019-07-03 10:04:45 -0600

    Added for loop

--- InFrameStopCodon.py
+++ InFrameStopCodon.py
@@ -3,7 +3,9 @@ def has_stop_codon(dna) :
     "This function checks if dna sequence has an in frame stop codon"
     stop_codon_found = False
     stop_codons = ['tga', 'tag', 'taa']
-    
+    for i in range(0,len(dna),3) :
+        codon = dna[i:i+3].lower()
+
 if(has_stop_codon(dna))
     print("Input sequence has a stop codon")
 else:
