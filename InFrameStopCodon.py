cded1d05964add628bcc51a24b6843b772513cc0
Sidhant Puntambekar <35539945+SidhantPuntambekar@users.noreply.github.com>
2019-07-03 10:00:30 -0600

    Started has_stop_codon function

--- InFrameStopCodon.py
+++ InFrameStopCodon.py
@@ -1,4 +1,7 @@
 dna = input("Enter a DNA sequence:")
+def has_stop_codon(dna) :
+    "This function checks if dna sequence has an in frame stop codon"
+    
 if(has_stop_codon(dna))
     print("Input sequence has a stop codon")
 else:
