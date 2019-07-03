88825bb0f8c9cc33eaa7719fd9fc9a67f3769334
Sidhant Puntambekar <35539945+SidhantPuntambekar@users.noreply.github.com>
2019-07-03 09:49:29 -0600

    Fixed styling issues

--- gcPercentageFunction.py
+++ gcPercentageFunction.py
@@ -1,5 +1,7 @@
 def gc(dna) :
 #This function computes the gc percentage of DNA
     numbases = dna.count('n')+dna.count('N')
-    gcPercent = float(dna.count('c')+dna.count('C')+dna.count('g')+dna.count('G'))
+    gcPercent = float(dna.count('c')+dna.count('C')+dna.count('g')+dna.count('G')*100/len(dna)-numbases)
+    return gcPercent
+print(gc('AAAGTNNAGTCC'))
     
