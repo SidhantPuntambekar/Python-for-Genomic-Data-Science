c32a4c69e302cd8065f16aa48e977b0a34e1f235
Sidhant Puntambekar <35539945+SidhantPuntambekar@users.noreply.github.com>
2019-07-08 09:09:59 -0600

    Added read file commands

--- GenomicDataScienceFinalProject.py
+++ GenomicDataScienceFinalProject.py
@@ -1,5 +1,14 @@
 class dna_tools ():
     def __init__(self,file_name) :
         self.file_name = file_name
-        self.dictionary = {}
-        f_reader = 
+        self.dict = {}
+        f_reader = open (self.file_name)
+        for line in f_reader:
+            line = line.strip("\n")
+            if ">" in line:
+                header = line
+                self.dict[header] = ""
+            else:
+                self.dict[header] = ""
+        f_reader.close
+    
